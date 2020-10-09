a2976e9daa01360fd5ef6ef6a5e2b6e51e8c04f7
Pietro <pietrosette@gmail.com>
2020-10-09 20:06:44 +0200

    prodotti.blade - css in progress

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>La Molisana</title>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    </head>
+    <body>
