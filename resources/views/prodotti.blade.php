e7ebc897fabdfe5f1bc78bf1fa301147dcc7460b
Pietro <pietrosette@gmail.com>
2020-10-11 10:11:17 +0200

    page prodotti.blade done

--- resources/views/prodotti.blade.php
+++ resources/views/prodotti.blade.php
@@ -157,26 +157,38 @@ foreach ($cards as $card) {
     <main>
         <div class="cards">
             <h2>Le lunghe</h2>
-            @foreach ($lunghe as $card)
-                <div class="card">
-                    <h3>{{$card['titolo']}}</h3>
-                    <img src="{{$card['src']}}" alt="{{$card['titolo']}}">
-                </div>
-            @endforeach
+            <div class="border-box">
+                @foreach ($corte as $card)
+                    <div class="card">
+                        <div class="box-card">
+                            <h3>{{$card['titolo']}}</h3>
+                            <img src="{{$card['src']}}" alt="{{$card['titolo']}}">
+                        </div>
+                    </div>
+                @endforeach
+            </div>
             <h2>Le corte</h2>
-            @foreach ($corte as $card)
-                <div class="card">
-                    <h3>{{$card['titolo']}}</h3>
-                    <img src="{{$card['src']}}" alt="{{$card['titolo']}}">
-                </div>
-            @endforeach
+            <div class="border-box">
+                @foreach ($corte as $card)
+                    <div class="card">
+                        <div class="box-card">
+                            <h3>{{$card['titolo']}}</h3>
+                            <img src="{{$card['src']}}" alt="{{$card['titolo']}}">
+                        </div>
+                    </div>
+                @endforeach
+            </div>
             <h2>Le cortissime</h2>
-            @foreach ($cortissime as $card)
-                <div class="card">
-                    <h3>{{$card['titolo']}}</h3>
-                    <img src="{{$card['src']}}" alt="{{$card['titolo']}}">
-                </div>
-            @endforeach
+            <div class="border-box">
+                @foreach ($corte as $card)
+                    <div class="card">
+                        <div class="box-card">
+                            <h3>{{$card['titolo']}}</h3>
+                            <img src="{{$card['src']}}" alt="{{$card['titolo']}}">
+                        </div>
+                    </div>
+                @endforeach
+            </div>
         </div>
     </main>
     @include('partials.footer')
